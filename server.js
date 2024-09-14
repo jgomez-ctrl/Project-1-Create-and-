a85890c90bb6676f7121de2c0de682007f12f779
jagomez88 <jasonlgomez88@gmai.com>
2024-09-14 18:24:55 -0400

    BAREBONES WORKING: buttons work, but is unclean and user creation is empty and takes no input from page

--- server.js
+++ server.js
@@ -2,9 +2,100 @@
 //testing git updating 
 //testing third line for git commits from ps in vscode
 
+const express = require('express'); //import express
+const app = express();              //create app instance
+const port = 3000;                  //define port number to run on
+
+// Middleware 
+app.use(express.json());
+app.use(express.static('public')); // Serve static files from 'public' folder
+
+// Sample data
+let users = [
+    { id: 1, fName: 'John', lName: 'Smith' },
+    { id: 2, fname: 'Samantah', lName: 'Doe' }
+];
+
+// Routes
+
+//
+//GET
+//
+//get requests for all  users
+app.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+//
+//POST
+//
+//create new user, add to array of users
+app.post('/api/users', (req, res) => {
+    const newUser = { id: users.length + 1, ...req.body };
+    users.push(newUser);
+    res.json(newUser);
+});
+
+app.get('/api/users/:id', (req, res) => {
+    const user = users.find(u => u.id == req.params.id);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+app.put('/api/users/:id', (req, res) => {
+    const user = users.find(searchUser => searchUser.id == req.params.id);
+    if (user) {
+        Object.assign(user, req.body);
+        res.json(user);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+app.delete('/api/users/:id', (req, res) => {
+    const userIndex = users.findIndex(searchUser => searchUser.id == req.params.id);
+    if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+        res.json({ message: 'User deleted' });
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* console.log("Hello World >.>"); */
 
-// Setup
+/* // Setup
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -56,4 +147,4 @@ app.use((req, res) => {
 // Tell the app to start listening for requests
 app.listen(HTTP_PORT, () => {
   console.log('Ready to handle requests on port ' + HTTP_PORT);
-});
+}); */
